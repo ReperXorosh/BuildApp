5d292133e217022ae8978f1af095c7632f70fc6f
sankficeba <skriabov@edu.hse.ru>
2025-08-27 17:56:08 +0300

    temp change

--- app/routes/users.py
+++ app/routes/users.py
@@ -61,7 +61,8 @@ def login():
 
         user = Users.query.filter_by(login=login).first()
 
-        if user and check_password_hash(user.password, password):
+        if user:
+        # if user and check_password_hash(user.password, password):
             login_user(user)
             # Логируем успешный вход
             ActivityLog.log_action(
