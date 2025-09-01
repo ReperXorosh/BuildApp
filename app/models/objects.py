14cb6ad472b4bfde1e65ac48aa572b5928e010fc
sankficeba <095senka095@gmail.com>
2025-09-02 00:46:29 +0300

    r

--- app/models/objects.py
+++ app/models/objects.py
@@ -139,7 +139,7 @@ class WorkExecution(db.Model):
     """Модель выполнения работы"""
     __tablename__ = 'work_executions'
     
-    work_executions_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column("work_executions_id", db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     planned_work_id = db.Column(db.String(36), db.ForeignKey('planned_works.planned_works_id'), nullable=False)
     execution_date = db.Column(db.Date, nullable=False)
     start_time = db.Column(db.Time)
