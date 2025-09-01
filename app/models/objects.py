80a8f33c033143bc0ee20d48c1f3bc1a30fc00bb
sankficeba <095senka095@gmail.com>
2025-09-02 01:04:03 +0300

    r

--- app/models/objects.py
+++ app/models/objects.py
@@ -115,7 +115,7 @@ class PlannedWork(db.Model):
     """Модель запланированной работы"""
     __tablename__ = 'planned_works'
     
-    id = db.Column("planned_works_id", db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     object_id = db.Column(db.String(36), db.ForeignKey('objects.id'), nullable=False)
     work_type = db.Column(db.String(100), nullable=False)  # 'support_installation', 'trench_excavation', etc.
     work_title = db.Column(db.String(255), nullable=False)
@@ -140,7 +140,7 @@ class WorkExecution(db.Model):
     __tablename__ = 'work_executions'
     
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    planned_work_id = db.Column(db.String(36), db.ForeignKey('planned_works.planned_works_id'), nullable=False)
+    planned_work_id = db.Column(db.String(36), db.ForeignKey('planned_works.id'), nullable=False)
     execution_date = db.Column(db.Date, nullable=False)
     start_time = db.Column(db.Time)
     end_time = db.Column(db.Time)
@@ -159,7 +159,7 @@ class WorkComparison(db.Model):
     __tablename__ = 'work_comparisons'
     
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    planned_work_id = db.Column(db.String(36), db.ForeignKey('planned_works.planned_works_id'), nullable=False)
+    planned_work_id = db.Column(db.String(36), db.ForeignKey('planned_works.id'), nullable=False)
     work_execution_id = db.Column(db.String(36), db.ForeignKey('work_executions.id'), nullable=False)
     comparison_date = db.Column(db.DateTime, default=datetime.utcnow)
     
