56d9812fa554ce2a4500a2b011c048a78340fb1e
ReperXorosh <kvartalius@gmail.com>
2025-10-10 01:03:34 +0300

    изменение страницы склада, полностью переделываю под специфику и удобство 1.5

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .settings import SystemSetting  # noqa: F401
 from .users import Users
 from .activity_log import ActivityLog
-from .supply import Material, Equipment, SupplyOrder, SupplyOrderItem
+from .supply import Material, Equipment, SupplyOrder, SupplyOrderItem, WarehouseMovement, WarehouseAttachment, UserMaterialAllocation, SupplyRequest, SupplyRequestItem
 from .objects import Object, Support, Trench, TrenchExcavation, TrenchFile, Report, Checklist, ChecklistItem
 from .remembered_device import RememberedDevice
