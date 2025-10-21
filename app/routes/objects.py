955c189567d30a56db3de486f773b35f36bfbc78
ReperXorosh <kvartalius@gmail.com>
2025-10-21 21:58:40 +0300

    исправление просмотра файлов в подробной информации о элементе

--- app/routes/objects.py
+++ app/routes/objects.py
@@ -966,10 +966,18 @@ def element_detail(object_id, element_type, element_id):
         element_id=element_id
     ).order_by(ElementAttachment.uploaded_at.desc()).all()
     
+    # Проверяем, есть ли старые файлы в заметках
+    old_file_url = None
+    if element.notes:
+        for line in element.notes.split('\n'):
+            if 'Файл:' in line:
+                old_file_url = line.replace('Файл:', '').strip()
+                break
+    
     from ..utils.mobile_detection import is_mobile_device
     is_mobile = is_mobile_device() or (request.args.get('mobile') == '1')
     template = 'objects/mobile_element_detail.html' if is_mobile else 'objects/element_detail.html'
-    return render_template(template, object=obj, element=element, element_type=title, attachments=attachments)
+    return render_template(template, object=obj, element=element, element_type=title, attachments=attachments, old_file_url=old_file_url)
 
 # Обновление статуса элемента
 @objects_bp.route('/api/objects/<uuid:object_id>/elements/<string:element_type>/<uuid:element_id>/status', methods=['PUT'])
